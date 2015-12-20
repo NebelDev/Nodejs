0c340d58fef90d56ac5b6f00460776d7d47ae1b1
NebelDev <NebelDev@users.noreply.github.com>
2015-12-20 18:23:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+var express = require('express');
+var app = express();
 
+var options = {
+  index: "index.html"
+};
+
+app.use(express.static(__dirname + '/html',options));
+
+app.listen(process.env.PORT || 3000);
